cedab7833dbe0f089a587663fefbf12fa39ce9f7
xotticaa-dot <xotticaa@gmail.com>
2026-02-02 14:39:27 -0700

    Add event listeners for yes and no buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesBtn.addEventListener("click", () => {
+alert("Yay! 💖");
+});
+
+noBtn.addEventListener("click", () => {
+alert("Oh no! 💔");
+});
